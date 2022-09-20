c5c8aafa7bddf3a506ae21d31907631a839bf7ea
Samuel Silva <samuel0100wanderson@gmail.com>
2022-09-20 13:27:35 -0300

    Ajuste automático de Hora/Minuto

--- Registrador version 0.1.py
+++ Registrador version 0.1.py
@@ -1,12 +1,14 @@
 from PySimpleGUI import PySimpleGUI as sg
-
+from datetime import datetime
 
 def registrador():
-
+    a = datetime.today().strftime('%H:%M')
+    b = int()
+    c = 'R$ 0,00'
     sg.theme('DarkPurple7')
     linha = [
         [sg.Text('Placa'), sg.Text('          Modelo/cor'), sg.Text('             Entrada'), sg.Text('  Saída'), sg.Text('  Valor do Serviço')],
-        [sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input('',size=(5, 1)), sg.Input('',size=(5,1)), sg.Input('',size=(14, 1))],
+        [sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input(a,size=(5, 1)), sg.Input(b,size=(5,1)), sg.Input(c,size=(14, 1))],
     ]
     layout2 = [
         [sg.Frame('Registrador', layout=linha, key='container')],
@@ -22,7 +24,10 @@ while True:
     if eventos2 == sg.WINDOW_CLOSED: #Permitindo que sja encerrado programa
         break
     elif eventos2 == 'Adicionar Registro':
-        janela2.extend_layout(janela2['container'], [[sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input('',size=(5, 1)), sg.Input('',size=(5,1)), sg.Input('',size=(14, 1))]])
+        a = datetime.today().strftime('%H:%M')
+        b = int()
+        c = 'R$ 0,00'
+        janela2.extend_layout(janela2['container'], [[sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input(a,size=(5, 1)), sg.Input(b,size=(5,1)), sg.Input(c,size=(14, 1))]])
     elif eventos2 == 'Resetar Registros':
         janela2.close()
         janela2 = registrador()
