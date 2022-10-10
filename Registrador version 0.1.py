1891834cef336d7bc48db0d26d9fb32d78f0e506
Samuel Silva <samuel0100wanderson@gmail.com>
2022-10-10 15:22:45 -0300

    Incrementação de Recursos

--- Registrador version 0.1.py
+++ Registrador version 0.1.py
@@ -2,11 +2,16 @@ from PySimpleGUI import PySimpleGUI as sg
 from datetime import datetime as dt
 
 def registrador():
+    layout_menu = [['Arquivo', ['Gerar Relatório', 'Encaminha para E-mail']],
+                   ['Opções', ['Alterar Tema', 'Voltar a Tela de Login']],
+                   ['Ajuda', ['Manual do Software', 'Contator Desenvolvedor',['E-mail: samuel0100wanderson@gmail.com', 'higor_scosta@outlook.com']]]]
+
     a = dt.today().strftime('%H:%M')
     b = int()
     c = 'R$ 0,00'
     sg.theme('DarkPurple7')
     linha = [
+        [sg.Menu(layout_menu)],
         [sg.Text('Placa'), sg.Text('          Modelo/cor'), sg.Text('             Entrada'), sg.Text('  Saída'), sg.Text('  Valor do Serviço')],
         [sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input(a,size=(5, 1)), sg.Input(b,size=(5,1)), sg.Input(c,size=(14, 1))],
     ]
@@ -14,13 +19,15 @@ def registrador():
         [sg.Frame('Registrador', layout=linha, key='container')],
         [sg.Button('Adicionar Registro'), sg.Button('Resetar Registros')]
     ]
-    return sg.Window('Registro de Veículos', layout=layout2, finalize=True)
+
+    return sg.Window('Registro de Veículos', layout=layout2, resizable=True, finalize=True)
 
 
 janela2 = registrador()
 # Regras da Janela
 while True:
-    eventos2, valores2 = janela2.read()
+    eventos2, valores2 = janela2.read(timeout=1)
+
     if eventos2 == sg.WINDOW_CLOSED: #Permitindo que seja encerrado programa
         break
     elif eventos2 == 'Adicionar Registro':
@@ -29,10 +36,11 @@ while True:
         c = 'R$ 0,00'
         janela2.extend_layout(janela2['container'], [[sg.Input('',size=(9, 1)), sg.Input('',size=(20, 1)), sg.Input(a,size=(5, 1)), sg.Input(b,size=(5,1)), sg.Input(c,size=(14, 1))]])
     elif eventos2 == 'Resetar Registros':
+        sg.theme('DarkPurple5')
         sim = 'Sim'
         nao = 'Não'
-        answer = sg.popup('Se confirmar isso irá apagar todos os registros!\nTem certeza?', custom_text=(sim, nao), title='Aviso')
-
+        answer = sg.popup('Se confirmar isso irá apagar todos os registros!\nTem certeza?', font='Arial', custom_text=(sim, nao), background_color= 'Brown', title='Aviso')
+        sg.theme('DarkPurple7')
         if answer == nao:
             ()
         elif answer == sim:
--- Tela de Login version 0.1.py
+++ Tela de Login version 0.1.py
@@ -1,9 +1,13 @@
 from PySimpleGUI import PySimpleGUI as sg
+
 #Janela 1
 #Layout
 def login():
     sg.theme('DarkPurple7')
+    menu = [['Ajuda', ['Acessar Manual', 'Contator Desenvolvedor',['E-mail: samuel0100wanderson@gmail.com', 'higor_scosta@outlook.com']]]]
     layout = [
+
+        [sg.Menu(menu)],
         [sg.Text('Usuário'), sg.Input(key='usuario', size=(30,1))],
         [sg.Text('  Senha'), sg.Input(key='senha',password_char='*', size=(30,1))],
         [sg.Checkbox('Salvar Login')],
@@ -18,9 +22,15 @@ def login():
     while True:
         eventos, valores = janela.read()
         if eventos == sg.WINDOW_CLOSED:
-            break
+            telinha = sg.popup('Deseja encerrar o processo de Login?', custom_text=('Sim', 'Não'), title= 'Aviso')
+            if telinha == 'Sim':
+                break
+            else: return login()
+
         elif eventos == 'Entrar': #SENHA: 19062012 POIS É A DATA DE INAUGURAÇÃO DA LOJA
             if (((valores['usuario'] == 'Albano Dias')) or ((valores['usuario'] == 'André Dias')) or ((valores['usuario'] == 'Cristiano Dias'))) and (valores['senha'] == '19062012'):
                 janela['msg'].update('Login realizado com sucesso!')
+            elif (valores['usuario'] == ('')) or (valores['senha'] == ('')):
+                janela['msg'].update('Preencha todas as Lacunas!')
             else: janela['msg'].update('Senha ou Usuário incorreto')
 login()
